8dcfc79df9be31f1445a6800800f2c8f1e89534c
Stig B. Dørmænen <stigbd@gmail.com>
2019-10-30 13:55:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
+
+# informationmodel-cat: Informationmodel Catalog Service  
+
+## Overview
+
+Module for harvesting and querying information models.
+
+## Data types
+
+### ApiDocument
---
+++ applications/informationmodel-cat/src/main/java/no/fdk/imcat/service/RegistrationApiClient.java
@@ -0,0 +1,18 @@
+package no.fdk.imcat.service;
+
+import no.fdk.registration.bindings.RegistrationApiBindings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RegistrationApiClient extends RegistrationApiBindings {
+    @Autowired
+    private Environment env;
+
+    @Override
+    public String getApiRootUrl() {
+        return env.getProperty("application.apiRootUrl");
+    }
+}
