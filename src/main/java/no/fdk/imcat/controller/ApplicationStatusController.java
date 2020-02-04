1b1c73715297b06f3264391bd9c232e8fbebf7cb
NilsOveTen <nils.ove.tendenes@acando.no>
2020-02-04 12:42:49 +0100

    chore: lombok rest & static Assertion import

--- src/main/java/no/fdk/imcat/controller/ApplicationStatusController.java
+++ src/main/java/no/fdk/imcat/controller/ApplicationStatusController.java
@@ -1,24 +1,20 @@
 package no.fdk.imcat.controller;
 
+import lombok.RequiredArgsConstructor;
 import no.fdk.imcat.service.InformationmodelRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class ApplicationStatusController {
     private static final Logger logger = LoggerFactory.getLogger(ApplicationStatusController.class);
 
-    private InformationmodelRepository informationmodelRepository;
-
-    @Autowired
-    public ApplicationStatusController(InformationmodelRepository informationmodelRepository) {
-        this.informationmodelRepository = informationmodelRepository;
-    }
+    private final InformationmodelRepository informationmodelRepository;
 
     @GetMapping("/ping")
     public ResponseEntity<Void> ping() {
