e90f944faea369b34c95989b7fe07ad6d67fe6bd
valosnah <exthos@brreg.no>
2020-04-06 15:12:18 +0200

    chore: added rabbitmq messaging for updating search

--- src/main/java/no/fdk/imcat/config/RabbitMQConfig.java
+++ src/main/java/no/fdk/imcat/config/RabbitMQConfig.java
@@ -6,6 +6,8 @@ import no.fdk.imcat.service.HarvestAdminClient;
 import no.fdk.imcat.service.RDFToModelTransformer;
 import no.fdk.imcat.service.RabbitMQListener;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,5 +43,12 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(TopicExchange topicExchange, Queue queue) {
         return BindingBuilder.bind(queue).to(topicExchange).with("informationmodel.*.HarvestTrigger");
+    }
+
+    @Bean
+    public AmqpTemplate jsonRabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 }
