66d6920daba60a82f24e5ce115233541eb2a7ffe
NilsOveTen <github.nils.ove@tendenes.com>
2020-10-13 13:30:43 +0200

    chore: enable CORS

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/controller/ApplicationStatusController.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/controller/ApplicationStatusController.kt
@@ -3,10 +3,12 @@ package no.fdk.fdk_informationmodel_harvester.controller
 import no.fdk.fdk_informationmodel_harvester.service.InformationModelService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class ApplicationStatusController(private val informationModelService: InformationModelService) {
 
     @GetMapping("/ping")
