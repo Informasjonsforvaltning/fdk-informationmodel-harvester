213ffe2613af44d5164b6b73b8a1ccffdaf19ea3
NilsOveTen <github.nils.ove@tendenes.com>
2020-10-13 13:30:43 +0200

    chore: enable CORS

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/controller/CatalogsController.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/controller/CatalogsController.kt
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest
 private val LOGGER = LoggerFactory.getLogger(CatalogsController::class.java)
 
 @Controller
+@CrossOrigin
 @RequestMapping(value = ["/catalogs"], produces = ["text/turtle", "text/n3", "application/rdf+json", "application/ld+json", "application/rdf+xml", "application/n-triples"])
 open class CatalogsController(private val informationModelService: InformationModelService) {
 
