456fdd5d1b9997cb75628083d2419a148bd22593
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-23 14:58:55 +0200

    chore: add forceUpdate to incoming rabbit messages

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/harvester/HarvesterActivity.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/harvester/HarvesterActivity.kt
@@ -29,24 +29,24 @@ class HarvesterActivity(
     private val activitySemaphore = Semaphore(1)
 
     @PostConstruct
-    private fun fullHarvestOnStartup() = initiateHarvest(null)
+    private fun fullHarvestOnStartup() = initiateHarvest(HarvestAdminParameters(null, null, null), false)
 
-    fun initiateHarvest(params: HarvestAdminParameters?) {
-        if (params == null) LOGGER.debug("starting harvest of all information models")
-        else LOGGER.debug("starting harvest with parameters $params")
+    fun initiateHarvest(params: HarvestAdminParameters, forceUpdate: Boolean) {
+        if (params.harvestAllModels()) LOGGER.debug("starting harvest of all information models, force update: $forceUpdate")
+        else LOGGER.debug("starting harvest with parameters $params, force update: $forceUpdate")
 
         launch {
             activitySemaphore.withPermit {
-                harvestAdminAdapter.getDataSources(params ?: HarvestAdminParameters())
+                harvestAdminAdapter.getDataSources(params)
                     .filter { it.dataType == "informationmodel" }
                     .filter { it.url != null }
-                    .map { async { harvester.harvestInformationModelCatalog(it, Calendar.getInstance()) } }
+                    .map { async { harvester.harvestInformationModelCatalog(it, Calendar.getInstance(), forceUpdate) } }
                     .awaitAll()
                     .filterNotNull()
                     .also { updateService.updateUnionModel() }
                     .also {
-                        if (params != null) LOGGER.debug("completed harvest with parameters $params")
-                        else LOGGER.debug("completed harvest of all catalogs") }
+                        if (params.harvestAllModels()) LOGGER.debug("completed harvest with parameters $params, force update: $forceUpdate")
+                        else LOGGER.debug("completed harvest of all catalogs, force update: $forceUpdate") }
                     .run { publisher.send(this) }
             }
         }
