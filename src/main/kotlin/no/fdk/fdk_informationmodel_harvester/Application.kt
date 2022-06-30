eb742dc72ee5a2f69ee10af9398be42ea3524c3e
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-30 09:42:42 +0200

    chore: replace deprecated security config

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/Application.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/Application.kt
@@ -4,10 +4,11 @@ import no.fdk.fdk_informationmodel_harvester.configuration.ApplicationProperties
 import no.fdk.fdk_informationmodel_harvester.configuration.FusekiProperties
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProperties::class, FusekiProperties::class)
+@EnableConfigurationProperties(ApplicationProperties::class, FusekiProperties::class, OAuth2ResourceServerProperties::class)
 open class Application
 
 fun main(args: Array<String>) {
