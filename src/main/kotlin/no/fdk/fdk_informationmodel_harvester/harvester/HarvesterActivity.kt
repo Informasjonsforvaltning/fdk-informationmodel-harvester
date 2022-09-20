888668ef122d856d9d4b02829ab566ebf8ab2522
NilsOveTen <github.nils.ove@tendenes.com>
2022-09-20 14:51:16 +0200

    fix: wrap semaphore activity with try-catch

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/harvester/HarvesterActivity.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/harvester/HarvesterActivity.kt
@@ -13,7 +13,7 @@ import no.fdk.fdk_informationmodel_harvester.rabbit.RabbitMQPublisher
 import no.fdk.fdk_informationmodel_harvester.service.UpdateService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.util.*
+import java.util.Calendar
 import javax.annotation.PostConstruct
 
 private val LOGGER = LoggerFactory.getLogger(HarvesterActivity::class.java)
@@ -24,7 +24,7 @@ class HarvesterActivity(
     private val harvester: InformationModelHarvester,
     private val publisher: RabbitMQPublisher,
     private val updateService: UpdateService
-): CoroutineScope by CoroutineScope(Dispatchers.Default) {
+) : CoroutineScope by CoroutineScope(Dispatchers.Default) {
 
     private val activitySemaphore = Semaphore(1)
 
@@ -37,17 +37,30 @@ class HarvesterActivity(
 
         launch {
             activitySemaphore.withPermit {
-                harvestAdminAdapter.getDataSources(params)
-                    .filter { it.dataType == "informationmodel" }
-                    .filter { it.url != null }
-                    .map { async { harvester.harvestInformationModelCatalog(it, Calendar.getInstance(), forceUpdate) } }
-                    .awaitAll()
-                    .filterNotNull()
-                    .also { updateService.updateUnionModel() }
-                    .also {
-                        if (params.harvestAllModels()) LOGGER.debug("completed harvest with parameters $params, force update: $forceUpdate")
-                        else LOGGER.debug("completed harvest of all catalogs, force update: $forceUpdate") }
-                    .run { publisher.send(this) }
+                try {
+                    harvestAdminAdapter.getDataSources(params)
+                        .filter { it.dataType == "informationmodel" }
+                        .filter { it.url != null }
+                        .map {
+                            async {
+                                harvester.harvestInformationModelCatalog(
+                                    it,
+                                    Calendar.getInstance(),
+                                    forceUpdate
+                                )
+                            }
+                        }
+                        .awaitAll()
+                        .filterNotNull()
+                        .also { updateService.updateUnionModel() }
+                        .also {
+                            if (params.harvestAllModels()) LOGGER.debug("completed harvest with parameters $params, force update: $forceUpdate")
+                            else LOGGER.debug("completed harvest of all catalogs, force update: $forceUpdate")
+                        }
+                        .run { publisher.send(this) }
+                } catch (ex: Exception) {
+                    LOGGER.error("harvest failure", ex)
+                }
             }
         }
     }
