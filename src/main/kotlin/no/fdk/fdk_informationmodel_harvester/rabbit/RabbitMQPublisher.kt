cb37696a1105077b8cb91b829f4e4f0af0f39b29
NilsOveTen <github.nils.ove@tendenes.com>
2022-11-14 07:53:34 +0100

    chore: rm publish of old rabbit message

--- src/main/kotlin/no/fdk/fdk_informationmodel_harvester/rabbit/RabbitMQPublisher.kt
+++ src/main/kotlin/no/fdk/fdk_informationmodel_harvester/rabbit/RabbitMQPublisher.kt
@@ -12,7 +12,6 @@ private val LOGGER = LoggerFactory.getLogger(RabbitMQPublisher::class.java)
 class RabbitMQPublisher(private val template: RabbitTemplate) {
     fun send(reports: List<HarvestReport>) {
         try {
-            template.convertAndSend("harvests","informationmodels.harvester.UpdateSearchTrigger", reports)
             template.convertAndSend("harvests","informationmodels.harvested", reports)
             LOGGER.debug("Successfully sent harvest completed message")
         } catch (e: AmqpException) {
